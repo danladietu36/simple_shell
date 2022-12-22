0a46f70bc324bc3e8dd1132637066b0de7a0ded8
Yusuf Abdul-hayyi <abdulhayyiyusuf2015@gmail.com>
2022-12-21 18:38:40 -0800

    code fix

---
+++ _strlen.c
@@ -0,0 +1,40 @@
+#include <stddef.h>
+
+/**
+ * _strlen - return the length of a string
+ * @s: A string
+ *
+ * Return: The length of the string
+ */
+size_t _strlen(char *s)
+{
+	size_t i = 0;
+
+	while (s[i++])
+		;
+	return (i);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"_strlen.c" [New File]                                        0,0-1         All
+
